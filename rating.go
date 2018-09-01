56b6044aee9aa20ab6cc5408eae490b300b1f230
DMXMax <29312988+DMXMax@users.noreply.github.com>
2018-09-01 05:32:16 +0000

    Update

--- rating.go
+++ rating.go
@@ -29,7 +29,7 @@ func processData(w http.ResponseWriter, body []byte){
   if json.Valid(body){
     w.Write([]byte("Body is Valid"))
     a := make(map[string]string)
-    err := json.Unmarshal(body, &a)
+    err := json.Unmarshal(body, a)
     if err != nil{
       w.Write([]byte("Success!"))
       w.Write([]byte(a["Safety"]))
